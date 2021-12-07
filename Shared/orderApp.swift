5d8ff17cfc90cc92af7fc218eadee17700999c65
Chen-RT <916300692@qq.com>
2021-12-08 02:03:38 +0800

    Initial Commit

---
+++ Shared/orderApp.swift
@@ -0,0 +1,17 @@
+//
+//  orderApp.swift
+//  Shared
+//
+//  Created by 陈润桐 on 2021/12/8.
+//
+
+import SwiftUI
+
+@main
+struct orderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
